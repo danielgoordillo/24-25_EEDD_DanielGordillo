de84025d87cf577b02839fe277f514358f3fa47f
dgordillog05 <dgordillog05@educarex.es>
2025-04-23 16:56:33 +0200

    3.5

--- Tema3/3.5/AnalizadorDatos/src/main/java/com/gordillo/daniel/analizadorDatos/AnalizadorNumerico.java
+++ Tema3/3.5/AnalizadorDatos/src/main/java/com/gordillo/daniel/analizadorDatos/AnalizadorNumerico.java
@@ -1,47 +1,37 @@
 package com.gordillo.daniel.analizadorDatos;
 
+import com.lavado.gonzalo.UtilidadesNumeros;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class AnalizadorNumerico {
 
     public boolean esPar(int numero) {
-        return numero % 2 == 0;
+        return UtilidadesNumeros.esPar(numero);
     }
 
     public boolean esImpar(int numero) {
-        return numero % 2 != 0;
+        return UtilidadesNumeros.esImpar(numero);
     }
 
     public boolean esPrimo(int numero) {
-        if (numero <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(numero); i++) {
-            if (numero % i == 0) return false;
-        }
-        return true;
+        return UtilidadesNumeros.esPrimo(numero);
+
     }
 
+
     public long factorial(int numero) {
-        long resultado = 1;
-        for (int i = 1; i <= numero; i++) {
-            resultado *= i;
-        }
-        return resultado;
+        return (long) UtilidadesNumeros.factorial(numero);
     }
 
     public List<Integer> devolverDivisores(int numero) {
-        List<Integer> divisores = new ArrayList<>();
-        for (int i = 1; i <= numero; i++) {
-            if (numero % i == 0) divisores.add(i);
-        }
-        return divisores;
+        return UtilidadesNumeros.devolverDivisores(numero);
     }
 
     public boolean esPerfecto(int numero) {
-        List<Integer> divisores = devolverDivisores(numero);
-        divisores.remove(divisores.size() - 1); // Eliminar el número mismo
-        int sumaDivisores = divisores.stream().mapToInt(Integer::intValue).sum();
-        return sumaDivisores == numero;
+        return UtilidadesNumeros.esPerfecto(numero);
     }
+
 }
 
--- Tema3/3.5/AnalizadorDatos/src/main/java/com/gordillo/daniel/analizadorDatos/AnalizadorTexto.java
+++ Tema3/3.5/AnalizadorDatos/src/main/java/com/gordillo/daniel/analizadorDatos/AnalizadorTexto.java
@@ -1,40 +1,27 @@
 package com.gordillo.daniel.analizadorDatos;
 
+import com.lavado.gonzalo.UtilidadesTexto;
+
 public class AnalizadorTexto {
 
     public boolean esPalindromo(String texto) {
-        String textoLimpio = texto.replaceAll("[^a-zA-Z]", "").toLowerCase();
-        String reverso = new StringBuilder(textoLimpio).reverse().toString();
-        return textoLimpio.equals(reverso);
+        return UtilidadesTexto.esPalindromo(texto);
     }
 
     public int contarVocales(String texto) {
-        int contador = 0;
-        for (char c : texto.toLowerCase().toCharArray()) {
-            if ("aeiou".indexOf(c) != -1) {
-                contador++;
-            }
-        }
-        return contador;
+        return UtilidadesTexto.contarVocales(texto);
     }
 
     public int contarConsonantes(String texto) {
-        int contador = 0;
-        for (char c : texto.toLowerCase().toCharArray()) {
-            if (Character.isLetter(c) && "aeiou".indexOf(c) == -1) {
-                contador++;
-            }
-        }
-        return contador;
+        return UtilidadesTexto.contarConsonantes(texto);
     }
 
     public int contarPalabras(String texto) {
-        String[] palabras = texto.trim().split("\\s+");
-        return palabras.length;
+        return UtilidadesTexto.contarPalabras(texto);
     }
 
     public int contarCaracteres(String texto) {
-        return texto.replaceAll(" ", "").length();
+        return UtilidadesTexto.contarCaracteres(texto);
     }
 }
 
